6aca24c54187c4c60f4a52c002c6befa7dff6fa4
chloeych <52624099+chloeych@users.noreply.github.com>
2020-04-29 10:16:56 -0400

    latest updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,121 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import {Route, BrowserRouter as Router, Redirect} from 'react-router-dom';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+//import pages
+import CreateAccount from './pages/CreateAccount.js'; 
+import Dashboard from  './pages/Dashboard.js'; 
+import Login from './pages/Login.js'; 
+
+//styling
 import './App.css';
 
+
 function App() {
-  return (
-    <header>Hello World!</header>
-  );
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true); 
+  const [userInformation, setUserInformation] = useState({});
+
+  const firebaseConfig = {
+  apiKey: "AIzaSyBV6DxXt1G7u6aHW-1devVzlbNdLx-GVq0",
+  authDomain: "final-project-dwa-d2cba.firebaseapp.com",
+  databaseURL: "https://final-project-dwa-d2cba.firebaseio.com",
+  projectId: "final-project-dwa-d2cba",
+  storageBucket: "final-project-dwa-d2cba.appspot.com",
+  messagingSenderId: "721781788829",
+  appId: "1:721781788829:web:14027a9031baae902b8474",
+};
+
+useEffect(()=>{
+  if(!firebase.apps.length){
+   firebase.initializeApp(firebaseConfig);
+ }
+ firebase
+ .auth()
+ .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+ .catch(function(e) {
+   console.log("AUTH ERROR",e);
+ });
+}, [firebaseConfig]);
+
+useEffect(()=> {
+  firebase.auth().onAuthStateChanged(function(user){
+    if(user){
+      setUserInformation(user);
+      setLoggedIn(true); 
+    }
+    else{
+      setLoggedIn(false);
+    }
+    setLoading(false);
+  });
+}, []);
+
+ // Logout 
+ function LogoutFunction(){
+  firebase
+  .auth()
+  .signOut()
+  .then(function(){
+    setLoggedIn(false)
+  })
+  .catch(function(error){
+    console.log(" LOGOUT ERROR", error);
+  });
 }
+
+// Create Account
+function CreateAccountFunction(e){
+  e.preventDefault(); 
+  console.log("form payload", e);  
+  let email = e.currentTarget.createEmail.value; 
+  let password = e.currentTarget.createPassword.value;
+
+  console.log('email', email); 
+  console.log("password", password);
+
+//Login
+function LoginFunction(e){
+  e.preventDefault(); 
+  let email = e.currentTarget.loginEmail.value;
+  let password = e.currentTarget.loginPassword.value;
+
+  firebase
+  .auth()
+  .signInWithEmailAndPassword(email,password)
+  .then(function(response){
+    console.log("LOGIN RESPONSE", response); 
+    setLoggedIn(true); 
+  })
+  .catch(function(error){
+    console.log("LOGIN ERROR", error)
+  });
+}
+
+if (loading) return null;
+
+  return (
+    <div className="App">
+      <Router>
+        <Route exact path="/">
+          <Dashboard/>
+        </Route>
+
+        <Route exact path="/login">
+       {!loggedIn ? (<Login LoginFunction={LoginFunction}/>) : (<Redirect to="/"/>)}
+       </Route>
+
+       <Route exact path="/create-account">
+       {!loggedIn ? (<CreateAccount CreateAccountFunction={CreateAccountFunction}/>) : (<Redirect to="/"/>)}
+       </Route>
+
+      </Router>
+
+
+    </div>
+  );
+}}
 
 export default App;
